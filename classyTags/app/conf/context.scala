a0fc932a4036959afd108ad421dbf9d1ae4918e7
Francis Carr <francis.carr@guardian.co.uk>
2014-07-15 12:54:29 +0100

    Add getPeople places and events to ClassyTags

--- classyTags/app/conf/context.scala
+++ classyTags/app/conf/context.scala
@@ -3,9 +3,13 @@ package conf
 import com.gu.management._
 import com.gu.management.play.{ Management => GuManagement }
 import model.Content
-import tags.Thing
+import tags.{Event, Place, Person, Thing}
 
-case class ClassyTag(item: Content, superTags: Seq[Thing])
+case class ClassyTag(item: Content, superTags: Seq[Thing]) {
+  def getPeople: Seq[Person] = superTags.collect { case p: Person => p}
+  def getPlaces: Seq[Place] = superTags.collect { case p: Place => p}
+  def getEvents: Seq[Event] = superTags.collect { case e: Event => e}
+}
 
 object Management extends GuManagement {
   val applicationName = "frontend-classy-tags"

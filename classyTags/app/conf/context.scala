8e545c740531df05a406a808a80c41c62343f499
Francis Carr <francis.carr@guardian.co.uk>
2014-07-15 14:38:44 +0100

    Add ClassyTags object with last minute scalaz hacking

--- classyTags/app/conf/context.scala
+++ classyTags/app/conf/context.scala
@@ -4,6 +4,9 @@ import com.gu.management._
 import com.gu.management.play.{ Management => GuManagement }
 import model.Content
 import tags.{Event, Place, Person, Thing}
+import scalaz._,Scalaz._
+import scalaz.syntax.monoid._
+import scalaz.std.map._
 
 case class ClassyTag(item: Content, superTags: Seq[Thing]) {
   def getPeople: Seq[Person] = superTags.collect { case p: Person => p}
@@ -13,6 +16,11 @@ case class ClassyTag(item: Content, superTags: Seq[Thing]) {
   def getPeopleGroupedByCategory: Map[String, Seq[Person]] = getPeople.groupBy(_.category)
   def getPlacesGroupedByCategory: Map[String, Seq[Place]] = getPlaces.groupBy(_.category)
   def getEventsGroupedByCategory: Map[String, Seq[Event]] = getEvents.groupBy(_.category)
+}
+
+object ClassyTag {
+  def reduceTheMap(tags: Seq[ClassyTag]) =
+    tags.map { _.getEventsGroupedByCategory }.reduce((a,b) => intersectWith(a, b)(_ ++ _))
 }
 
 object Management extends GuManagement {
